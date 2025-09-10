6512f4ae237020ca614632cd5fd57c6f20003ff6
Aaron Granath TE24C <divedconh287@gmail.com>
2025-09-10 07:16:48 +0200

    fixade svar på tåget

--- TE24C_Quiz/Program.cs
+++ TE24C_Quiz/Program.cs
@@ -1,10 +1,15 @@
 int points = 0;
-Console.WriteLine("Välkommen till frågesport!!! skriv endast bokstaven");
+Console.WriteLine("skriv ditt namn");
+string namn;
+Console.WriteLine($"Välkommen till frågesport{namn}!!! skriv endast bokstaven");
+namn = Console.ReadLine().ToLower();
+
 Console.WriteLine("Fråga 1:\n");
 Console.WriteLine("a) b) c)");
 string answer1 = Console.ReadLine().ToLower();
 if (answer1 == "b")
 {
+    points++;
     Console.WriteLine("CORRECT");
 }
 else
@@ -17,6 +22,7 @@ Console.WriteLine("a) b) c)");
 string answer2 = Console.ReadLine().ToLower();
 if (answer2 == "b")
 {
+    points++;
 Console.WriteLine("CORRECT");
 }
 else
@@ -29,12 +35,30 @@ Console.WriteLine("a) b) c)");
 string answer3 = Console.ReadLine().ToLower();
 if (answer3 == "b")
 {
+    points++;
     Console.WriteLine("CORRECT");
 }
 else
 {
     Console.WriteLine("INCORRECT");
 }
-Console.WriteLine("GOOD JOB YOU GOT uhhh POINTS");
+Console.WriteLine($"GOOD JOB YOU GOT {points} POINTS");
+if (points == 0)
+{
+    Console.WriteLine("boooo");
+}
+else if (points == 1)
+{
+    Console.WriteLine("okej");
+}
+else if (points == 2)
+{
+    Console.WriteLine("Det är \"dos\" på spanska, förvånad att du inte visste det.");
+}
+else
+{
+    Console.WriteLine("ALLA?!\n");
+}
 
+Console.WriteLine("ok hejdå");
 Console.ReadLine();
--- TE24C_Quiz/obj/Debug/net9.0/TE24C_Quiz.AssemblyInfo.cs
+++ TE24C_Quiz/obj/Debug/net9.0/TE24C_Quiz.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TE24C_Quiz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc03d075446b2d773f6398aabc44856718269cb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+435c4c53321a43ad715d0d3d970e5a3a1436b495")]
 [assembly: System.Reflection.AssemblyProductAttribute("TE24C_Quiz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TE24C_Quiz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
