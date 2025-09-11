29c2dd0cd04b003f89fd27dd10a4e0308ae5b686
Aaron Granath TE24C <divedconh287@gmail.com>
2025-09-11 08:24:45 +0200

    vet inte vad som ändrades

--- TE24C_Quiz/obj/Debug/net9.0/TE24C_Quiz.AssemblyInfo.cs
+++ TE24C_Quiz/obj/Debug/net9.0/TE24C_Quiz.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TE24C_Quiz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a100853ce9752ab008410c68d8dbfab156f7c6bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a406d6e305ddbb9694e0124917ebc23a244abb5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TE24C_Quiz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TE24C_Quiz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
